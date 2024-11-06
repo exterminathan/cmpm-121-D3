8fa0f4a5144addbed29033c467b9c0048fb88b00
Nathan Shturm <exterminathan@gmail.com>
2024-11-05 23:59:42 -0800

    re-test commit

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
 const button = document.createElement("button");
 button.textContent = "Click";
 
-button.addEventListener('click', () => {
-    alert('you clicked the button!');
-})
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
 
-
+//add to doc
 document.body.append(button);
