43181595d7465f2ec8898d83e71cd89387fa3aed
Nathan Shturm <exterminathan@gmail.com>
2024-11-05 23:56:54 -0800

    L16 Init Commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+//create button
+const button = document.createElement("button");
+button.textContent = "Click";
+
+button.addEventListener('click', () => {
+    alert('you clicked the button!');
+})
+
+
+document.body.append(button);
