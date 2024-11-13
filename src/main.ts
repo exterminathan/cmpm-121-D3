c85c54049ff712f1b2bae4809eb0a226581cba8d
Nathan Shturm <exterminathan@gmail.com>
2024-11-13 01:20:36 -0800

    will it work this time?

--- src/main.ts
+++ src/main.ts
@@ -45,20 +45,16 @@ const map = leaflet.map("map", {
 
 //custom dark map
 leaflet
-  .tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-    {
-      minZoom: 0,
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    } as leaflet.TileLayerOptions,
-  )
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
   .addTo(map);
 
 //custom player icon
 const playerIconCustom = leaflet.icon({
-  iconUrl: `src/assets/locIcon.png`,
+  iconUrl: `./src/assets/locIcon.png`,
   iconSize: [35, 49],
   iconAnchor: [17.5, 24.5],
   popupAnchor: [0, -24.5],
@@ -92,21 +88,21 @@ let visibleCacheKeys = new Set<string>();
 
 // custom geocache icons
 const geocacheIconSmall = leaflet.icon({
-  iconUrl: `src/assets/marker64.png`,
+  iconUrl: `./src/assets/marker64.png`,
   iconSize: [24, 24],
   iconAnchor: [12, 12],
   popupAnchor: [0, -12],
 });
 
 const geocacheIconMed = leaflet.icon({
-  iconUrl: `src/assets/marker64.png`,
+  iconUrl: `./src/assets/marker64.png`,
   iconSize: [32, 32],
   iconAnchor: [16, 16],
   popupAnchor: [0, -16],
 });
 
 const geocacheIconLarge = leaflet.icon({
-  iconUrl: `src/assets/marker64.png`,
+  iconUrl: `./src/assets/marker64.png`,
   iconSize: [40, 40],
   iconAnchor: [20, 20],
   popupAnchor: [0, -20],
