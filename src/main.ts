42c9831486df29a7e0a770056e5a6d3e7e72e126
Nathan Shturm <exterminathan@gmail.com>
2024-11-13 01:24:02 -0800

    test map

--- src/main.ts
+++ src/main.ts
@@ -45,11 +45,15 @@ const map = leaflet.map("map", {
 
 //custom dark map
 leaflet
-  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  })
+  .tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    {
+      minZoom: 0,
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    } as leaflet.TileLayerOptions,
+  )
   .addTo(map);
 
 //custom player icon
